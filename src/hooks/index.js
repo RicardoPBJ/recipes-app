ce06a621f5e563458b24ee228b715c2f7525088a
ezio <eziopborges@gmail.com>
2023-01-26 16:03:38 -0300

    fetch geral

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as Provider } from './context/ContextProvider';
 export { default as Context } from './context/createContext';
 export { default as handlers } from './custom/Handlers';
+export { default as makeFetch } from './custom/makeFetch';
