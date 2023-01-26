8afbf857596ae000d5a894d001508ffb40807454
ezio <eziopborges@gmail.com>
2023-01-26 15:19:10 -0300

    construção das pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,7 @@
 export { default as Login } from './Login';
 export { default as Recipes } from './Recipes';
+export { default as DoneRecipes } from './DoneRecipes';
+export { default as Drinks } from './Drinks';
+export { default as Meals } from './Meals';
+export { default as Profile } from './Profile';
+export { default as FavoriteRecipes } from './FavoriteRecipes';
