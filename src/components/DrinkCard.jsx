10adb76f309defeb004209ad0246f208a09a76cb
marllonwagner <marllonwagner91@gmail.com>
2023-01-30 15:51:59 -0300

    req 20 done

--- src/components/DrinkCard.jsx
+++ src/components/DrinkCard.jsx
@@ -2,13 +2,22 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default function DrinkCard({ drinkData }) {
+  const TWELVE = 12;
+  const drinkData12 = drinkData.slice(0, TWELVE);
   return (
     <div>
       {
-        drinkData.map((e) => (
-          <div key={ e.idDrink }>
-            <img src={ e.strDrinkThumb } alt={ e.strGlass } />
-            <p>{ e.strGlass }</p>
+        drinkData12.map((e, index) => (
+          <div
+            data-testid={ `${index}-recipe-card` }
+            key={ e.idDrink }
+          >
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ e.strDrinkThumb }
+              alt={ e.strDrink }
+            />
+            <p data-testid={ `${index}-card-name` }>{ e.strDrink }</p>
           </div>
 
         ))
@@ -20,5 +29,6 @@ export default function DrinkCard({ drinkData }) {
 DrinkCard.propTypes = {
   drinkData: PropTypes.shape({
     map: PropTypes.func.isRequired,
+    slice: PropTypes.func.isRequired,
   }).isRequired,
 };
--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -2,14 +2,23 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 function MealCard({ mealData }) {
-  console.log('comida nhami nhami', mealData);
+  const TWELVE = 12;
+  const mealData12 = mealData.slice(0, TWELVE);
+
   return (
     <div>
       {
-        mealData.map((e) => (
-          <div key={ e.idMeal }>
-            <img src={ e.strMealThumb } alt={ e.strMeal } />
-            <p>{ e.strMeal }</p>
+        mealData12.map((e, index) => (
+          <div
+            data-testid={ `${index}-recipe-card` }
+            key={ e.idMeal }
+          >
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ e.strMealThumb }
+              alt={ e.strMeal }
+            />
+            <p data-testid={ `${index}-card-name` }>{ e.strMeal }</p>
           </div>
 
         ))
@@ -21,6 +30,7 @@ function MealCard({ mealData }) {
 MealCard.propTypes = {
   mealData: PropTypes.shape({
     map: PropTypes.func,
+    slice: PropTypes.func,
   }).isRequired,
 };
 
