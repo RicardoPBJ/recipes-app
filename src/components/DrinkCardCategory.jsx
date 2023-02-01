836b713a3235badd49db0fe0da6831ed533e781e
marllonwagner <marllonwagner91@gmail.com>
2023-02-01 15:58:17 -0300

    req 19-23 faltam os testes

--- src/components/DrinkCardCategory.jsx
+++ src/components/DrinkCardCategory.jsx
@@ -1,39 +1,39 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-function DrinkCardCategory({ items }) {
-  // const TWELVE = 12;
-  // const drinksRecipesToShow = items.drinks.slice(0, TWELVE);
-  return (
-    <div>
-      {
-        items.map((e, index) => (
-          <div
-            data-testid={ `${index}-recipe-card` }
-            key={ e.idDrink }
-          >
-            <Link to={ `/drinks/${e.idDrink}` }>
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ `${e.strDrinkThumb}/preview` }
-                alt={ e.strDrink }
-              />
-            </Link>
-            <p data-testid={ `${index}-card-name` }>{ e.strDrink }</p>
-          </div>
-        ))
+// function DrinkCardCategory({ items }) {
+//   // const TWELVE = 12;
+//   // const drinksRecipesToShow = items.drinks.slice(0, TWELVE);
+//   return (
+//     <div>
+//       {
+//         items.map((e, index) => (
+//           <div
+//             data-testid={ `${index}-recipe-card` }
+//             key={ e.idDrink }
+//           >
+//             <Link to={ `/drinks/${e.idDrink}` }>
+//               <img
+//                 data-testid={ `${index}-card-img` }
+//                 src={ `${e.strDrinkThumb}/preview` }
+//                 alt={ e.strDrink }
+//               />
+//             </Link>
+//             <p data-testid={ `${index}-card-name` }>{ e.strDrink }</p>
+//           </div>
+//         ))
 
-      }
-    </div>
-  );
-}
+//       }
+//     </div>
+//   );
+// }
 
-DrinkCardCategory.propTypes = {
-  items: PropTypes
-    .shape({ drinks: PropTypes
-      .arrayOf(PropTypes.objectOf(PropTypes.string)) }).isRequired,
+// DrinkCardCategory.propTypes = {
+//   items: PropTypes
+//     .shape({ drinks: PropTypes
+//       .arrayOf(PropTypes.objectOf(PropTypes.string)) }).isRequired,
 
-};
+// };
 
-export default DrinkCardCategory;
+// export default DrinkCardCategory;
