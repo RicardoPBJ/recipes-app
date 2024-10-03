a35a460179b241cd749b601720d164d629cc4d69
RicardoPBJ <rbrumjunio@gmail.com>
2024-10-03 16:58:59 -0300

    ajustando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import profileIcon from '../images/profileIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
+import { IoSearch } from 'react-icons/io5';
+import { AiOutlineUser } from 'react-icons/ai';
 import SearchBar from './SearchBar';
 import '../styles/App.css';
 
@@ -10,40 +10,42 @@ export default function Header() {
   const { pathname } = useLocation();
 
   return (
-    <main className="headerfix">
-      <header className="headerfix">
+    <div className="container">
+      <header className="headerfix align-items-center justify-content-evenly">
         {
           !/^\/profile|\/done|\/favorite/i.test(pathname)
           && (
             <button
-              style={ { background: 'none', border: 'none', marginLeft: '90px' } }
+              style={ { background: 'none', border: 'none' } }
               onClick={ () => setSearch(!search) }
             >
-              <img
-                src={ searchIcon }
+              <IoSearch
+                className="iconSrch"
+                size={ 40 }
                 alt="profile-icon"
                 data-testid="search-top-btn"
               />
             </button>
           )
         }
-        <h1 data-testid="page-title">
+        <h1 data-testid="page-title" className="text-white">
           {[...pathname.matchAll(/(?<g1>\b[a-z])(?<g2>(?!\b[a-z])[a-z]+)/g)]
             .reduce((title, { groups: { g1, g2 } }) => (
               title ? `${title} ${g1.toUpperCase()}${g2}` : `${g1.toUpperCase()}${g2}`
             ), '')}
         </h1>
         <Link to="/profile">
-          <img
+          <AiOutlineUser
             alt="profile-icon"
             data-testid="profile-top-btn"
-            src={ profileIcon }
+            size={ 40 }
+            color="white"
           />
         </Link>
       </header>
       {
         search && <SearchBar />
       }
-    </main>
+    </div>
   );
 }
