fed621fefde4ac26546596d915e418f09147a4ed
RicardoPBJ <rbrumjunio@gmail.com>
2024-10-10 18:50:23 -0300

    alterando tamanho e como o header é divido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,37 +11,40 @@ export default function Header() {
 
   return (
     <div className="container">
-      <header className="headerfix align-items-center justify-content-evenly">
-        {
-          !/^\/profile|\/done|\/favorite/i.test(pathname)
-          && (
-            <button
-              style={ { background: 'none', border: 'none' } }
-              onClick={ () => setSearch(!search) }
-            >
-              <IoSearch
-                className="iconSrch"
-                size={ 40 }
-                alt="profile-icon"
-                data-testid="search-top-btn"
-              />
-            </button>
-          )
-        }
-        <h1 data-testid="page-title" className="text-white">
+      <header className="headerfix align-items-center justify-content-between">
+        <h1 data-testid="page-title" className="text-white header-title">
           {[...pathname.matchAll(/(?<g1>\b[a-z])(?<g2>(?!\b[a-z])[a-z]+)/g)]
             .reduce((title, { groups: { g1, g2 } }) => (
               title ? `${title} ${g1.toUpperCase()}${g2}` : `${g1.toUpperCase()}${g2}`
             ), '')}
         </h1>
-        <Link to="/profile">
-          <AiOutlineUser
-            alt="profile-icon"
-            data-testid="profile-top-btn"
-            size={ 40 }
-            color="white"
-          />
-        </Link>
+        <div className="d-flex">
+          {
+            !/^\/profile|\/done|\/favorite/i.test(pathname)
+          && (
+            <button
+              style={ { background: 'none', border: 'none', marginRight: '2em' } }
+              onClick={ () => setSearch(!search) }
+            >
+              <IoSearch
+                className="icons container"
+                size={ 55 }
+                alt="search-icon"
+                data-testid="search-top-btn"
+              />
+            </button>
+          )
+          }
+          <Link to="/profile">
+            <AiOutlineUser
+              className="icons"
+              alt="profile-icon"
+              data-testid="profile-top-btn"
+              size={ 55 }
+              color="white"
+            />
+          </Link>
+        </div>
       </header>
       {
         search && <SearchBar />
